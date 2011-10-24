7a687ba3487d75ed3d2b166caf7428cdd60d7a6b
Maximilian <Fox56@gmx.de>
2011-10-24 11:18:13 +0200

    basic file structure

---
+++ app/wikimap_basic.rb
@@ -0,0 +1,17 @@
+#encoding: utf-8
+
+################################################################
+## A command-line tool for testing the logical modules #########
+################################################################
+
+#ask for query string
+puts "WikiMap 0.0.1"
+puts "============="
+print "Bitte Phrase eingeben: "
+search_str = gets
+
+#research logic goes here
+#...
+
+#response
+puts search_str
