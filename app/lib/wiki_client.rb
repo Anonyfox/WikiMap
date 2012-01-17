ce1d0679ccc0fa4ae201cf817c664ae491cd35d3
Maximilian Stroh <Fox56@gmx.de>
2012-01-17 11:39:20 +0100

    bla

--- app/lib/wiki_client.rb
+++ app/lib/wiki_client.rb
@@ -32,7 +32,7 @@ module WikiClient
 	end
 
 	def self.output phrase, links=[], img_counter=0, destination=nil, thumbnail=true
-		destination ||= "tmp/my_graph_#{img_counter}.png"
+		destination ||= "./tmp/my_graph_#{img_counter}.png"
 		graph = GraphvizSimple.new("MindMap")
 		graph.edge_attributes = {"arrowhead" => "vee"}
 		graph.graph_attributes = {"bgcolor"=>"transparent"}
